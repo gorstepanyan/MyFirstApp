b92c1362b624ccc83436485bca2501c116cd3698
ASUS <ASUS@DESKTOP-9NT3G79>
2023-02-07 12:32:39 -0800

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press Enter to start working");
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+                Console.WriteLine("Starting my console exersise...");
+            else
+                return;
+
+            Console.Write("Pleace, type your Name: ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"Your name is {s}");
+
+            Console.Write("Press 1 to print 'Hello World' or 2 to print 'Hello People' : ");
+            char r = (char)Console.Read();
+            switch (r)
+            {
+                case '1':
+                    Console.WriteLine("Hello World");
+                    break;
+                case '2':
+                    Console.WriteLine("Hello People");
+                    break;
+                default:
+                    Console.WriteLine("Bye!!!");
+                    break;
+            };
+        }
+    }
+}
